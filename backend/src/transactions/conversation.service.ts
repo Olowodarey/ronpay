b62a68e8e0091be8ec07b7fa261806be83c350c0
caxtonacollins <caxtonacollins@gmail.com>
2026-02-17 01:19:14 +0100

    chore: clean up

--- backend/src/transactions/conversation.service.ts
+++ backend/src/transactions/conversation.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+    import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, LessThan } from 'typeorm';
 import { Conversation } from './entities/conversation.entity';
